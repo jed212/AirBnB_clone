e5c309eba81770cff7612fac70ece003db38df94
mogbonna <mogbonna045@gmail.com>
2023-12-09 13:06:37 -0800

    Changed the mode of some files and fix errors

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User."""
+    """User class that inherits from BaseModel."""
 
     email = ""
     password = ""
